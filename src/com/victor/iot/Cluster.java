be6fcf40bea85c91c9d29781dd55719f8a4c770e
Victor Wen <vic7tor@gmail.com>
2013-04-25 16:42:46 +0800

    developing cluster

--- src/com/victor/iot/Cluster.java
+++ src/com/victor/iot/Cluster.java
@@ -1,6 +1,11 @@
 package com.victor.iot;
 
 public class Cluster {
+	public static final int INT = 1;
+	public static final int BOOL = 2;
+	
 	int id;
+	String desc;
+	int type;
 	
 }
--- src/com/victor/iotgateapp/EndpointActivity.java
+++ src/com/victor/iotgateapp/EndpointActivity.java
@@ -1,20 +1,25 @@
 package com.victor.iotgateapp;
 
+import com.victor.iot.Endpoint;
 import com.victor.iot.IGateway;
 
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.app.Activity;
 import android.app.Service;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.view.Menu;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class EndpointActivity extends Activity {
 	private int nwkaddr;
 	private int epindex;
 	private IGateway gateway;
+	private Endpoint endpoint;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +27,25 @@ public class EndpointActivity extends Activity {
 		setContentView(R.layout.activity_endpoint);
 		
 		nwkaddr = getIntent().getIntExtra("nwkaddr", 0);
-		epindex = getIntent().getIntExtra("index", -1);
+		epindex = getIntent().getIntExtra("endpoint", -1);
 		
         Intent intent = new Intent();
         intent.setAction("com.victor.iot.GATEWAY");
         bindService(intent, conn, Service.BIND_AUTO_CREATE);
 	}
 
+	private void initActivity()
+	{
+		TextView nwkaddr = (TextView) findViewById(R.id.nwkaddr);
+		TextView pid = (TextView) findViewById(R.id.profileid);
+		TextView did = (TextView) findViewById(R.id.deviceid);
+		ListView list = (ListView) findViewById(R.id.listView);
+		
+		nwkaddr.setText(String.format("%04x", endpoint.nwkaddr));
+		pid.setText(String.format("%d", endpoint.profileid));
+		did.setText(String.format("%d", endpoint.deviceid));
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -43,6 +60,14 @@ public class EndpointActivity extends Activity {
 		public void onServiceConnected(ComponentName arg0, IBinder arg1) {
 			// TODO Auto-generated method stub
 			gateway = IGateway.Stub.asInterface(arg1);
+			try {
+				endpoint = gateway.getEndpoint(nwkaddr, epindex);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			initActivity();
 		}
 
 		@Override
